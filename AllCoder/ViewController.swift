0bc5cdec91ce0ae7521b76d268c7e3155560ae06
Shoutokobayashi <deepstrap353@gmail.com>
2019-09-07 11:37:59 +0900

    Initial Commit

---
+++ AllCoder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllCoder
+//
+//  Created by 小林聖人 on 2019/09/07.
+//  Copyright © 2019 小林聖人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
