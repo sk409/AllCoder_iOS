428972ce44c9aa0f9a1c89bb62582f7748cddb4b
Shoutokobayashi <deepstrap353@gmail.com>
2019-10-03 21:00:11 +0900

    test

--- ProMark/AppDelegate.swift
+++ ProMark/AppDelegate.swift
@@ -9,7 +9,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        //Auth.shared.logout()
         window = UIWindow()
         window?.makeKeyAndVisible()
         if Auth.shared.isLoggedIn() {
